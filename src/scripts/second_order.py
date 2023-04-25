6066a39eaf7bb508bba36b1a24bf278cfd93204a
Andrew McCluskey <andrew.mccluskey@ess.eu>
2023-04-25 08:16:41 +0200

    Set a title

--- src/scripts/second_order.py
+++ src/scripts/second_order.py
@@ -44,7 +44,7 @@ axes[-1].set_ylabel('$1/A(t)$')
 axes[-1].set_xlabel('$t$/s')
 
 axes.append(fig.add_subplot(gs[0, 1]))
-# titles.append("Linear plot")
+titles.append("Non-linear plot")
 axes[-1].errorbar(t, A[:, 1], A[:, 1], marker='.', color=fp.colors[1], label='$A$', zorder=10)
 axes[-1].set_ylabel('$A(t)$')
 axes[-1].set_xlabel('$t$/s')
