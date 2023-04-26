930f163e8b11fa8984a238636f507dd9c2d8e5fc
Andrew McCluskey <andrew.mccluskey@ess.eu>
2023-04-26 12:49:35 +0200

    Distributions plot

---
+++ src/scripts/distributions.py
@@ -0,0 +1,59 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from tueplots import figsizes
+import matplotlib.gridspec as gridspec
+from scipy.stats import norm
+
+import paths
+import _fig_params as fp
+
+rng = np.random.default_rng(1)
+
+distribution = rng.normal(loc=50, scale=5, size=int(2 ** 15))
+
+figsize = figsizes.icml2022_half(nrows=1, ncols=3, height_to_width_ratio=0.8)['figure.figsize']
+fig = plt.figure(figsize=figsize)
+gs = gridspec.GridSpec(1, 2, figure=fig, wspace=0.4, hspace=0.6)
+
+axes = []
+titles = []
+
+axes.append(fig.add_subplot(gs[0, 0]))
+titles.append("a")
+y, x = np.histogram(distribution, bins=fp.NBINS, density=True)
+axes[-1].stairs(y, x, color=fp.colors[2], alpha=0.5, fill=True)
+axes[-1].set_xlabel('$y$')
+axes[-1].set_ylabel('$p(y)$')
+# axes[-1].set_title('Non-linear fit')
+# axes[-1].set_xticks([0.8, 1, 1.2])
+
+axes.append(fig.add_subplot(gs[0, 1]))
+titles.append("b")
+y, x = np.histogram(1 / distribution, bins=fp.NBINS, density=True)
+axes[-1].stairs(y, x, color=fp.colors[0], alpha=0.5, fill=True)
+axes[-1].set_xlabel('$1 / y$')
+axes[-1].set_ylabel('$P(1 / y)$')
+# axes[-1].set_title('Linear fit')
+# axes[-1].set_xticks([1, 2, 3, 4])
+
+axes.append(fig.add_subplot(gs[0, 2]))
+titles.append("b")
+y, x = np.histogram(np.log(distribution), bins=fp.NBINS, density=True)
+axes[-1].stairs(y, x, color=fp.colors[1], alpha=0.5, fill=True)
+axes[-1].set_xlabel('$\ln{(y)}$')
+axes[-1].set_ylabel('$P[\ln{(y)}]$')
+# axes[-1].set_title('Linear fit')
+# axes[-1].set_xticks([1, 2, 3, 4])
+
+fig.align_ylabels(axes)
+
+x_correction = [15, 15, 15, 15]
+for i, ax in enumerate(axes):
+    x = ax.get_window_extent().x0 - x_correction[i]
+    y = ax.get_window_extent().y1 + 10
+    x, y = fig.transFigure.inverted().transform([x, y])
+    fig.text(x, y, titles[i], ha='left', fontweight='bold')
+
+# plt.figlegend(loc='upper center', bbox_to_anchor=(0.5, -0.01), ncol=3)
+plt.savefig(paths.figures / "distributions.pdf")
+plt.close()
