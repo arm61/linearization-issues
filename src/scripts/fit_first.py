0ac258004a684ce50a881eca1b5283f05189d759
Andrew McCluskey <andrew.mccluskey@ess.eu>
2023-04-28 07:33:28 +0200

    Fix formatting

--- src/scripts/fit_first.py
+++ src/scripts/fit_first.py
@@ -104,11 +104,11 @@ f.close()
 
 f = open(paths.output / 'lin_ci.txt', 'w')
 ci_lin = np.percentile(k_lin / k, [2.5, 97.5])
-f.write(r'\SIrange{' + f'{ci_lin[0]:.1e}' + r'}{' + f'{ci_lin[1]:1e}' + r'}{\per\second}')
+f.write(r'\SIrange{' + f'{ci_lin[0]:.1e}' + r'}{' + f'{ci_lin[1]:.1e}' + r'}{\per\second}')
 f.close()
 f = open(paths.output / 'non_ci.txt', 'w')
 ci_non = np.percentile(k_non / k, [2.5, 97.5])
-f.write(r'\SIrange{' + f'{ci_non[0]:.1e}' + r'}{' + f'{ci_non[1]:1e}' + r'}{\per\second}')
+f.write(r'\SIrange{' + f'{ci_non[0]:.1e}' + r'}{' + f'{ci_non[1]:.1e}' + r'}{\per\second}')
 f.close()
 
 fig.align_ylabels(axes)
